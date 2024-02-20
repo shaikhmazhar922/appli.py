115f67198646e777715170f9ee1ca4625abe4fbd
shaikhmazhar922 <159419063+shaikhmazhar922@users.noreply.github.com>
2024-02-20 21:59:17 +0530

    appli.py

--- appli.py
+++ appli.py
@@ -8,9 +8,6 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 
 st.title("SUPER MARKET DASHBOARD")
 st.markdown("##")
-
-    
-    
 df = pd.read_excel(
         io="supermarkt_sales.xlsx",
         engine="openpyxl",
@@ -20,19 +17,11 @@ df = pd.read_excel(
         usecols="B:R",
         nrows=300,
     )
-
-
-
-
 # ---- SIDEBAR ----
 
 with st.sidebar:
     st.subheader("Description")
     st.write("A supermarket is a self-service shop offering a wide variety of food, beverages and household products, organized into sections. This kind of store is larger and has a wider selection than earlier grocery stores, but is smaller and more limited in the range of merchandise than a hypermarket or big-box market.Data sources: Supermarket data comes from various sources such as point-of-sale (POS) systems, customer loyalty programs, inventory management systems, and online platforms. This data includes transaction records, product details, customer demographics, purchase history, and more.")
-
-
-
-
 
 st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
@@ -84,17 +73,8 @@ with left_column:
 
 with left_column:
     st.line_chart(df[['Rating','Unit price']])
-    
-    
-
 
 st.markdown("""---""")
-
-
-
-
-
-
 
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
@@ -110,8 +90,6 @@ fig_product_sales = px.bar(
     template="plotly_white",
 )
 
-
-
 fig_product_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
@@ -136,46 +114,12 @@ fig_product_sales = px.bar(
     template="plotly_white",
 )
 
-
-
 fig_product_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_product_sales, use_container_width=True)
-
-
-
-
-
-
-
-
-
-
 
 hide_st_style = """
             <style>
@@ -186,7 +130,19 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+pie chart of Sales by segment
+    with col2:
+        st.subheader("Sales by Segment")
+        category_sales = filtered_df.groupby('Segment')['Sales'].sum().reset_index()
+        fig = px.pie(category_sales, values='Sales', names='Segment')
+        st.plotly_chart(fig, use_container_width=True)
 
+pie chart of gross income by City
+    with col2:
+        st.subheader("gross income by City")
+        category_sales = filtered_df.groupby('City')['gross income'].sum().reset_index()
+        fig = px.pie(category_sales, values='gross income', names='City')
+        st.plotly_chart(fig, use_container_width=True)
 
 
 
